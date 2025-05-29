7466caeca2e8c67bc3924ba07642e045d747cd8e
arthursetragni <arthursetragni@gmail.com>
2025-05-29 07:33:51 -0300

    Configurando servidor para aceitar cliente em codigo C tirando TCP e colocando HTTP

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 const express = require('express');
-const net = require('net');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const server = net.createServer((socket) => {
-  console.log('Cliente conectado');
-
-  socket.on('data', (data) => {
-    console.log('Mensagem recebida do cliente:', data.toString());
-
-    // Responder ao cliente
-    socket.write('Mensagem recebida com sucesso!');
-  });
-
-  socket.on('end', () => {
-    console.log('Cliente desconectado');
-  });
-
-  socket.on('error', (err) => {
-    console.error('Erro:', err.message);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Servidor TCP rodando na porta ${PORT}`);
-});
-
-// Rota GET /receberMensagem
 app.get('/receberMensagem', (req, res) => {
+  console.log('Requisição recebida de um cliente HTTP');
   res.send('Mensagem recebida com sucesso!');
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor HTTP rodando em http://localhost:${PORT}`);
 });
