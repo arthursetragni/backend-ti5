53162b98c493e90eea4b87e51a8b246786a0685a
arthursetragni <arthursetragni@gmail.com>
2025-05-29 07:24:05 -0300

    Configurando servidor para aceitar cliente em codigo C

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const net = require('net');
 const app = express();
 const PORT = 3000;
+
+const server = net.createServer((socket) => {
+  console.log('Cliente conectado');
+
+  socket.on('data', (data) => {
+    console.log('Mensagem recebida do cliente:', data.toString());
+
+    // Responder ao cliente
+    socket.write('Mensagem recebida com sucesso!');
+  });
+
+  socket.on('end', () => {
+    console.log('Cliente desconectado');
+  });
+
+  socket.on('error', (err) => {
+    console.error('Erro:', err.message);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor TCP rodando na porta ${PORT}`);
+});
 
 // Rota GET /receberMensagem
 app.get('/receberMensagem', (req, res) => {
