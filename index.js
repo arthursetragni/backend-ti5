6d34d0082f4db0acee6b23be3167969ff2686324
arthursetragni <arthursetragni@gmail.com>
2025-05-29 11:14:52 -0300

    Configurando HTTPS da railway

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/receberMensagem', (req, res) => {
   res.send('Mensagem recebida com sucesso!');
 });
 
+app.get('/', (req, res) => {
+  console.log('Requisição recebida de um cliente HTTP');
+  res.send('Mensagem recebida com sucesso!');
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor HTTP rodando em http://localhost:${PORT}`);
 });
