242b679c07b44be32da27c866ce72c949000d06c
arthursetragni <arthursetragni@gmail.com>
2025-06-04 17:45:59 -0300

    const em tudo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv')
+const cors = require('cors')
 
-import dotenv from 'dotenv';
-import cors from 'cors';
 
 // import statsRoutes from './routes/statsRoutes.js';
 // import deviceRoutes from './routes/deviceRoutes.js';
